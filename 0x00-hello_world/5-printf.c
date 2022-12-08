42b78365922a60f5df5028b2c8afaf2a457510d4
Paul <paulemmanuelmaduka@gmail.com>
2022-12-08 04:40:08 -0800

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Print a string
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: prints "with proper grammar, but the outcome is a piece of art"
  * Return: 0
  */
 int main(void)
