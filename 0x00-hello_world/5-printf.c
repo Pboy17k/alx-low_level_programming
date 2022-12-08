417304652161dd9fa3275e937b8771239582085b
Paul <paulemmanuelmaduka@gmail.com>
2022-12-08 04:18:23 -0800

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /*
- *   main - Entry point
+ * main - Print a string
  *   Description: use printf
- *   My hello, printf task
- *   Return: return 1 after program execution
+ *   Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
