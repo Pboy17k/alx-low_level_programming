f15052c5ab62ce593171fa1d87d5fd1b905e74a1
Paul <paulemmanuelmaduka@gmail.com>
2022-12-10 00:09:36 -0800

    lpl

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,35 +3,29 @@
 
 
 /**
- *  * main - Entry point
- *    * Description: print all aplhabet letters
- *      * Return: Always 0 (Success)
+ *  * main - prints the alphabet in lowercase,
+ *   * followed by a new line
+ * Return: Always 0 (Success)
  */
-
-
 
 int main(void)
 
 {
 
-		char ch = 'a';
+		char ch;
 
 
 
-		while (ch <='z')
+	for (ch = 'a'; ch <= 'z'; ch++)
 
 					{
 
 								putchar(ch);
 
-										++ch;
-
-											}
+									}
 
 				putchar('\n');
-
-
-
+}
 					return (0);
 
-}
+
