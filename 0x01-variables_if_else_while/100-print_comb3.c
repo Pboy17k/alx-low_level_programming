db37dc8e748a9ba7d284750c48f819888e26fc6e
Paul <paulemmanuelmaduka@gmail.com>
2022-12-12 09:01:09 -0800

    p;p

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,37 +1,22 @@
 #include <stdio.h>
-
-
-
 /**
  * main - Entry point
  * Return: Always 0 (Succes)
  */
-
 int main(void)
-
 {
-
 	int c, i;
 
-
-
 	for (c = '0'; c <= '9'; c++)
-
 	{
-
 		for (i = '0'; i <= '9'; i++)
-
 		{
-
 			if (c < i)
 
 			{
 
 				putchar(c);
-
 				putchar(i);
-
-
 
 				if ((c != '8') || (c == '8' && i != '9'))
 
@@ -40,20 +25,12 @@ int main(void)
 					putchar(',');
 
 					putchar(' ');
-
 				}
-
 			}
-
 		}
-
 	}
 
-
-
 	putchar('\n');
-
-
 
 	return (0);
 
