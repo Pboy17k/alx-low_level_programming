4c765ad5a0abb59dfb81a42af6cd5661d57e1912
Paul <paulemmanuelmaduka@gmail.com>
2022-12-12 08:16:21 -0800

    olp

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,13 @@ int main(void)
 {
 	int c, i;
 
-	for (c = '0'; c <= '9'; c++)
+		for (c = '0'; c <= '9'; c++)
 
 	{
 
 		for (i = '0'; i <= '9'; i++)
 		{
-		if (c < i)
+			if (c < i)
 													{
 														putchar(c);
 														putchar(i);
@@ -27,7 +27,7 @@ int main(void)
 														{
 					putchar(',');
 					putchar(' ');								}
-																						
+
 																}
 
 									}
