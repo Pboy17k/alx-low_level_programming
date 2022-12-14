1373770fce2826cab6305cccb25303e723c71000
Paul <paulemmanuelmaduka@gmail.com>
2022-12-14 05:14:30 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,32 +1,13 @@
-/*
- * File: 1-alphabet.c
- * Auth: Mr Paul Emmanuel
- */
-
-
-
 #include "main.h"
-
-
-
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 
 void print_alphabet(void)
-
 {
+	char letter;
 
-		char alpha;
-
-
-
-			for (alpha = 97; alpha <= 122; alpha++)
-
-						_putchar(alpha);
-
-
-
-				_putchar('\n');
-
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
