dc20167e498854b73f568500e9e69b3e68450b83
Paul <paulemmanuelmaduka@gmail.com>
2022-12-15 11:35:04 -0800

    po

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+/**
+ * print_numbers - print 012345678
+ * *Return: void
+ */
+
+
+
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+i_putchar(c);
+}
+
+_putchar('\n');
+
+}
