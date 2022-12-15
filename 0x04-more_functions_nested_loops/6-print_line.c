02134d660b378791aeb25ae5a12bc5ce3cceb52c
Paul <paulemmanuelmaduka@gmail.com>
2022-12-15 12:02:55 -0800

    pop

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,14 +3,14 @@
 
 
 /*
- *  * print_line - printing lines
- *   * @n: number of times
- *     * Return: void
+ * print_line - printing lines
+ * @n: number of times
+ * Return: void
  */
 void print_line(int n)
 {
 
-	int i;
+int i;
 for (i = 0; i < n; i++)
 {
 _putchar('_');
