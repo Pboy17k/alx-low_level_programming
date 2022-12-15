f964a03d46ff942781f27e0f28e9abec65e997e6
Paul <paulemmanuelmaduka@gmail.com>
2022-12-15 11:14:49 -0800

    ol

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,18 +9,16 @@
 int _isupper(int c)
 
 {
-
-	if (c >= 'A' && c <= 'Z')
-	{
-	return (1);
-	}
-
-	else
+if (c >= 'A' && c <= 'Z')
 {
+return (1);
+}
 
-	return (0);
-
+else
 {
+return (0);
+
+}
 
 
 
