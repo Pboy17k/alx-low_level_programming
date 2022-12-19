a25fb15593672d5a2fd568ac7228fbe949505079
Paul <paulemmanuelmaduka@gmail.com>
2022-12-19 14:37:57 -0800

    p

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,18 +16,18 @@ while (str[len] != '\0')
 len++;
 }
 
-        if (len % 2 == 0)
-        {
-                for (i = len / 2; str[i] != '\0'; i++)
-                {
-                        putchar(str[i]);
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\0'; i++)
+{
+putchar(str[i]);
                 }
-        } else if (len % 2)
-        {
-                for (n = (len - 1) / 2; n < len - 1; n++)
-                {
-                        putchar(str[n + 1]);
-                }
-        }
-        putchar('\n');
+} else if (len % 2)
+{
+for (n = (len - 1) / 2; n < len - 1; n++)
+{
+putchar(str[n + 1]);
+}
+}
+putchar('\n');
 }
